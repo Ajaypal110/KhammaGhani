a85b5dc30cb7122d13783fbf3249554e79458e7a
unknown <ajaypalsingh82775@gmail.com>
2026-03-02 19:04:06 +0530

    Fix database validation crash for Razorpay and restore live deploy URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import RestaurantLogin from "./pages/RestaurantLogin";
 import RestaurantDashboard from "./pages/RestaurantDashboard";
 import RestaurantProtected from "./components/RestaurantProtected";
 import Profile from "./pages/Profile";
+import Cart from "./pages/Cart";
 import { CartProvider } from "./context/CartContext";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -44,6 +45,7 @@ function App() {
         />
         <Route path="/restaurant/:id" element={<RestaurantDetails />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/dish/:id" element={<DishDetails />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
