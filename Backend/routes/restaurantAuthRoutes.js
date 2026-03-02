538dc5215a997c268d90ec3c38b0b163063c80e0
unknown <ajaypalsingh82775@gmail.com>
2026-03-02 12:32:36 +0530

    Fix file casing for Linux/Render deployment

--- Backend/routes/restaurantAuthRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { restaurantLogin } from "../controllers/restaurantAuthController.js";
-
-const router = express.Router();
-
-router.post("/login", restaurantLogin);
-
-export default router;
--- Backend/routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { searchDish } from "../controllers/searchController.js";
-
-const router = express.Router();
-
-router.get("/search", searchDish);
-
-export default router;
